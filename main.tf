89bad8f8f6fd614ef3740516ea8760c6c73f3e5f
Pathiraja Mudiyanselage Anuradha Kumara Wickramasinghe <anuradha@192-168-1-101.tpgi.com.au>
2020-10-11 10:53:02 +1100

    added aws_ecr and aws_ecs resources to main.tf and tested infra creation

--- main.tf
+++ main.tf
@@ -2,3 +2,13 @@
 provider "aws" {
     region = "us-east-1"
 }
+
+#create AWS ecr repo to manage servian docker image
+resource "aws_ecr_repository" "servian_ecr_repo" {
+  name = "servian-ecr-repo" # Naming repo
+}
+
+#Create AWS ecs cluster to deploy docker container
+resource "aws_ecs_cluster" "servian_ecs_cluster" {
+  name = "servian-ecs-cluster" # Naming the cluster
+}
