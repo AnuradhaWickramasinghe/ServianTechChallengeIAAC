2bcb38f84aa7b17f0114df37d86f0194e6b14a57
Pathiraja Mudiyanselage Anuradha Kumara Wickramasinghe <anuradha@192-168-1-101.tpgi.com.au>
2020-10-11 12:08:59 +1100

    added aws iam_role with ecs_task_execution_policy and updated ecs task definition with task execution IAM role

--- main.tf
+++ main.tf
@@ -38,4 +38,28 @@ resource "aws_ecs_task_definition" "first_task" {
   network_mode             = "awsvpc"   
   memory                   = 512         # memory for container 
   cpu                      = 256         # CPU for container 
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+
+}
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+
+
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
