60b5ba79a799a8350479dafe62c6c8e87a238354
Pathiraja Mudiyanselage Anuradha Kumara Wickramasinghe <anuradha@192-168-1-101.tpgi.com.au>
2020-10-11 12:22:21 +1100

    added references to default VPC and subnets

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 #initiating provider
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1" #Todo remove the hard coded value
 }
 
 #create AWS ecr repo to manage servian docker image
@@ -14,9 +14,11 @@ resource "aws_ecs_cluster" "servian_ecs_cluster" {
 }
 
 
-#Creating AWS ecs task definition to run servian docker container on
+#Creating AWS ecs task definition to run servian docker container
+#Todo remove the hard coded value "3000" and make it parameter prompt on terraform apply
+
 resource "aws_ecs_task_definition" "first_task" {
-  family                   = "first-task" 
+  family                   = "first-task"
   container_definitions    = <<DEFINITION
   [
     {
@@ -35,9 +37,9 @@ resource "aws_ecs_task_definition" "first_task" {
   ]
   DEFINITION
   requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"   
-  memory                   = 512         # memory for container 
-  cpu                      = 256         # CPU for container 
+  network_mode             = "awsvpc"
+  memory                   = 512 # memory for container 
+  cpu                      = 256 # CPU for container 
   execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
 
 }
@@ -46,6 +48,7 @@ resource "aws_iam_role" "ecsTaskExecutionRole" {
   assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
 }
 
+#aws policy document with aws to execute ecs task_definition
 data "aws_iam_policy_document" "assume_role_policy" {
 
 
@@ -59,7 +62,25 @@ data "aws_iam_policy_document" "assume_role_policy" {
   }
 }
 
+#attachnig aws policy document to aws managed ploicy
 resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# make reference to  default VPC
+resource "aws_default_vpc" "default_vpc" {
+}
+
+# make reference to  default subnets
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "us-east-1a" #Todo remove the hard coded value
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "us-east-1b" #Todo remove the hard coded value
+}
+
+resource "aws_default_subnet" "default_subnet_c" {
+  availability_zone = "us-east-1c" #Todo remove the hard coded value
 }
