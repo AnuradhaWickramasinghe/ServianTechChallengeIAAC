c7cb46f4a54139978c11f3b690aa23b9c6acb131
Pathiraja Mudiyanselage Anuradha Kumara Wickramasinghe <anuradha@192-168-1-101.tpgi.com.au>
2020-10-12 01:13:36 +1100

    added aws security groups for alb and application service and added alb , alb_target group , alb lstner

--- main.tf
+++ main.tf
@@ -84,3 +84,91 @@ resource "aws_default_subnet" "default_subnet_b" {
 resource "aws_default_subnet" "default_subnet_c" {
   availability_zone = "us-east-1c" #Todo remove the hard coded value
 }
+
+#Creating aws ALB
+resource "aws_alb" "application_load_balancer" {
+  name               = "app-lb-tf" # Naming our load balancer
+  load_balancer_type = "application"
+  subnets = [ # Referencing the default subnets
+    "${aws_default_subnet.default_subnet_a.id}",
+    "${aws_default_subnet.default_subnet_b.id}",
+    "${aws_default_subnet.default_subnet_c.id}"
+  ]
+  # Referencing the security group
+  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+}
+
+#Creating aws ecs service to manage first-task
+resource "aws_ecs_service" "first_service" {
+  name            = "first-service"
+  cluster         = "${aws_ecs_cluster.servian_ecs_cluster.id}"
+  task_definition = "${aws_ecs_task_definition.first_task.arn}"
+  launch_type     = "FARGATE"
+  desired_count   = 3 #Todo remove the hard coded value
+
+  network_configuration {
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
+    assign_public_ip = true
+
+  }
+}
+
+resource "aws_security_group" "service_security_group" {
+  name = "service-security-group"
+  ingress {
+    from_port = 3000
+    to_port   = 3000
+    protocol  = "TCP"
+    # Only allowing traffic in from the load balancer security group
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+  }
+
+  egress {
+    from_port   = 0             
+    to_port     = 0             
+    protocol    = "-1"          
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+}
+
+# Creating a security group for the load balancer:
+resource "aws_security_group" "load_balancer_security_group" {
+  name = "load-balancer-security-group"
+  ingress {
+    from_port   = 80 
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0             
+    to_port     = 0             
+    protocol    = "-1"          
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#Creating aws application load balance target group
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group"
+  port        = 3000
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = "${aws_default_vpc.default_vpc.id}" # Referencing the default VPC
+  health_check {
+    matcher = "200,301,302"
+    path    = "/"
+  }
+}
+
+#Create aws ALB listner to listen 80 port and forward them to crated target group runs on port 3000
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" # Referencing our load balancer
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our tagrte group
+  }
+}
