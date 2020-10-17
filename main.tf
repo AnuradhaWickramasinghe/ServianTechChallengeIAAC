6e2e1527b30bb046f88adc753787f0b2ab9b48ca
Pathiraja Mudiyanselage Anuradha Kumara Wickramasinghe <anuradha@192-168-1-101.tpgi.com.au>
2020-10-17 17:21:43 +1100

    fix bug exist on output variable ecr_registry_id

--- main.tf
+++ main.tf
@@ -270,7 +270,7 @@ output "ecr_repository_url" {
 }
 
 output "ecr_registry_id" {
-  value = aws_ecr_repository.servian_ecr_repo.ecr_registry_id
+  value = aws_ecr_repository.servian_ecr_repo.registry_id
 }
 
 
