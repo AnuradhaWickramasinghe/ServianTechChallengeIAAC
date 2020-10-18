193677ba94557a5ff4df257ce9106b214ce98868
Pathiraja Mudiyanselage Anuradha Kumara Wickramasinghe <anuradha@192-168-1-101.tpgi.com.au>
2020-10-19 06:25:29 +1100

    the script updated to set environment variables in permanantly

--- set_env.sh
+++ set_env.sh
@@ -1,14 +1,13 @@
-#!/bin/sh
-export VTT_DBHOST=`terraform output db_connection_endpoint`
-export alb_dns_name=`terraform output alb_dns_name`
-export ecr_repo_name=`terraform output ecr_repo_name`
-export ecr_repository_url=`terraform output ecr_repository_url`
-export ecr_registry_id=`terraform output ecr_repository_url | cut -d '/' -f1`
- 
+#!/bin/bash
+echo "export VTT_DBHOST=$(terraform output db_connection_endpoint)" >> /etc/profile.d/setVars.sh
+echo "export alb_dns_name=$(terraform output alb_dns_name)" >> /etc/profile.d/setVars.sh
+echo "export ecr_repo_name=$(terraform output ecr_repo_name)" >> /etc/profile.d/setVars.sh
+echo "export ecr_repository_url=$(terraform output ecr_repository_url)" >> /etc/profile.d/setVars.sh
+echo "export ecr_registry_id=$(terraform output ecr_repository_url | cut -d '/' -f1)" >> /etc/profile.d/setVars.sh
+
 
 echo $VTT_DBHOST
 echo $alb_dns_name
 echo $ecr_repo_name
 echo $ecr_repository_url
 echo $ecr_registry_id
-
